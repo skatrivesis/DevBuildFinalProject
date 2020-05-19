897d556149b00c4f78f279bffb14708ef7ec8dd2
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-05-19 15:25:26 -0400

    fixed up sql schema

--- DevBuildFinalProject/CardKnightDBSchema.sql
+++ DevBuildFinalProject/CardKnightDBSchema.sql
@@ -1,50 +1,55 @@
---create database CardKnight
---go
---use CardKnight
---go
---create table Character (
---	Id int identity(1,1) primary key not null,
---	CharName nvarchar(20) not null,
---	Hp int not null,
---	Ap int not null,
---	Progress int not null,
---	Player bit not null
---);
---go
---create table Card (
---	Id int identity(1,1) primary key not null,
---	CardName varchar(20) not null,
---	Cost int not null,
---	Info varchar(120) not null,
---	Modifier int not null,
---	CardType varchar(20) not null
---);
---go
---create table Deck (
---	Id int identity(1,1) primary key not null,
---	CharId int foreign key references Character(Id),
---	CardId int foreign key references Card(Id)
---);
---go
---create table Equipment (
---	Id int identity(1,1) primary key not null,
---	EquipName varchar(30) not null,
---	Info varchar(120) not null,
---	Modifier int not null,
---	EquipType varchar(20) not null
---);
+create database CardKnight
+go
+use CardKnight
+go
+create table Character (
+	Id int identity(1,1) primary key not null,
+	CharName nvarchar(20) not null,
+	Hp int not null,
+	Ap int not null,
+	Progress int not null,
+	Player bit not null
+);
+go
+create table Card (
+	Id int identity(1,1) primary key not null,
+	CardName varchar(20) not null,
+	Cost int not null,
+	Info varchar(120) not null,
+	Modifier int not null,
+	CardType varchar(20) not null
+);
+go
+create table Deck (
+	Id int identity(1,1) primary key not null,
+	CharId int foreign key references Character(Id),
+	CardId int foreign key references Card(Id)
+);
+go
+create table Equipment (
+	Id int identity(1,1) primary key not null,
+	EquipName varchar(30) not null,
+	Info varchar(120) not null,
+	Modifier int not null,
+	EquipType varchar(20) not null
+);
+go
 
+--dummy data
+insert into card (CardName, Cost, Info, Modifier, CardType) 
+values ('Slash', 2, 'Standard sword slash', 3, 'Attack'),
+('Wild Swing', 1, 'Wildly swing around', 2, 'Attack')
+go
 
-----dummy data
---insert into card (CardName, Cost, Info, Modifier, CardType) 
---values ('Slash', 2, 'Standard sword slash', 3, 'Attack'),
---('Wild Swing', 1, 'Wildly swing around', 2, 'Attack')
+insert into Character (CharName, Hp, Ap, Progress, Player)
+values ('Soc', 20, 3, 0, 1),
+('Brandon', 20, 3, 0, 1),
+('O', 20, 3, 0, 1) 
+go
 
---insert into Character (CharName, Hp, Ap, Progress, Player)
---values ('Steve', 20, 3, 0, 1)
-
---insert into deck (CharId, CardId)
---values (1,2)
+insert into deck (CharId, CardId)
+values (1,2)
+go
 
 ----Return Char with id 1 and all cards associated
 
@@ -53,3 +58,7 @@
 --	left join Card on Card.Id = Deck.CardId
 --	left join Character on Character.Id = Deck.CharId
 --where CharId = 1
+--go
+
+----Return card by id
+--select * from card where id = 1
