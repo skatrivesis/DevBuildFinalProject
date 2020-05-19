4fd245b081709a305adf73300639f8835519d63a
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-05-19 10:30:14 -0400

    adding dal

---
+++ DevBuildFinalProject/DevBuildFinalProject/Services/CardDataService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildFinalProject.Services
+{
+    public class CardDataService : ICardDataService
+    {
+        private readonly string connString;
+
+        public CardDataService(IConfiguration config)
+        {
+            connString = config.GetConnectionString("default");
+        }
+
+        
+    }
+}
---
+++ DevBuildFinalProject/DevBuildFinalProject/Services/ICardDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildFinalProject.Services
+{
+    public interface ICardDataService
+    {
+        //return all cards
+
+        //return deck
+
+        //return card by id
+    }
+}
