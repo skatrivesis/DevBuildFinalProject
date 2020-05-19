c88e7433aa7838a238742c92021f772642f8900b
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-05-19 12:04:35 -0400

    working on getting character component and main menu component coded

---
+++ DevBuildFinalProject/DevBuildFinalProject/ClientApp/src/app/character/character.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { CharacterComponent } from './character.component';
+
+let component: CharacterComponent;
+let fixture: ComponentFixture<CharacterComponent>;
+
+describe('character component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ CharacterComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(CharacterComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ DevBuildFinalProject/DevBuildFinalProject/ClientApp/src/app/character/character.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-character',
+    templateUrl: './character.component.html',
+    styleUrls: ['./character.component.scss']
+})
+/** character component*/
+export class CharacterComponent {
+    /** character ctor */
+    constructor() {
+
+    }
+}
